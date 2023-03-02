b94a42372540a0457f0cb06909363699ae4e363e
YareliB07 <yarelibravo84@gmail.com>
2023-03-02 12:48:08 -0600

    actualiza netflix-app.py

--- netflix-app.py
+++ netflix-app.py
@@ -1,30 +1,36 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import codecs
 
+
+#Config. de pagina
 st.title('Netflix App')
 name_link ='movies.csv'
-st.text('Yareli Sugey Bravo Morales - zS20006784')
+
+st.text('Yareli Sugey Bravo Morales - ZS20006773')
+
 
 @st.cache
-def load_data(nrows):
-    name_link = codecs.open('movies.csv', mode='r', encoding=None, errors='strict', buffering=- 1)
 
+def load_data(nrows):
+    name_link = codecs.open("movies.csv",'r','latin1')
     data = pd.read_csv(name_link, nrows=nrows)
     return data
 
-data_load_state = st.text('cargando')
-data = load_data(500)
-data_load_state.text('netflix app!! (using st.cache)')
 
-st.dataframe(data)
-
-
+#Contenido del sidebar
 sidebar = st.sidebar
 st.sidebar.image("https://raw.githubusercontent.com/YareliB07/streamlit/master/credencial.jpeg")
 sidebar.title("Filtro")
-agree = st.sidebar.checkbox("Mostrar todos los filmes")
+
+agree=sidebar.checkbox("¿Desea ver los films recuperados?")
 if agree:
-  load_data(500)
-sidebar.write("Nombre de la pelicula que desea buscar:")
-sidebar.text_input("movie: ")
+    data_load_state = st.text('cargando')
+    data = load_data(500)
+    data_load_state.text('netflix app!! (using st.cache)')
+    st.dataframe(data)
+
+sidebar.markdown("##")
+
+title=sidebar.text_input("Ingresa el titulo de un pelicula:")
+agree=sidebar.button
